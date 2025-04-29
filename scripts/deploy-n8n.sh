2efb2ce0578dcd1ac70af9ebf4e87798cf91125a
subjec2change <97814728+subjec2change@users.noreply.github.com>
2025-04-29 12:09:02 -0500

    Update deploy-n8n.sh

--- scripts/deploy-n8n.sh
+++ scripts/deploy-n8n.sh
@@ -7,6 +7,6 @@ docker volume create caddy_data
 docker volume create n8n_data
 
 # Start Docker Compose
-docker compose -f config/docker-compose.yml up -d
+docker-compose -f config/docker-compose.yml up -d
 
 echo "n8n and Caddy deployed successfully."
